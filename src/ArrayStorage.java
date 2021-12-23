1259f0eeba27bde14adbd37d4cf1a15062343206
MaksimBS <BosUssur@gmail.com>
2021-12-23 17:35:01 +0300

    Решение первого задания

---
+++ src/ArrayStorage.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+            }
+        size = 0;
+    }
+
+    void save(Resume resume) {
+        storage[size] = resume;
+        size ++;
+    }
+
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    void delete(String uuid) {
+        boolean find = false;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid) || find) {
+                find = true;
+                storage[i]=storage[i+1];
+            }
+        }
+        size --;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        Resume[] copy = new Resume[size];
+        System.arraycopy(storage, 0, copy, 0, size);
+        return copy;
+    }
+
+    int size() {
+        return size;
+    }
+}
