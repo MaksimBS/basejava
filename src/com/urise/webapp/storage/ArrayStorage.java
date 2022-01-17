520e13bfc3d946d10572f9984e787aa5458747b4
MaksimBS <BosUssur@gmail.com>
2022-01-17 23:12:20 +0300

    update ArrayStorage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,27 +2,27 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] allResume = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            allResume[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void save(Resume resume) {
-        try {
-            getIndex(resume.getUuid());
-            System.out.println("This resume is already in the bank");
-        } catch (Exception e) {
-            if (size < allResume.length) {
-                allResume[size] = resume;
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            System.out.println("This resume " + resume.getUuid() + " is already in the bank");
+        } else {
+            if (size < storage.length) {
+                storage[size] = resume;
                 size++;
             } else {
                 System.out.println("Resume bank is full");
@@ -31,35 +31,32 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        try {
-            return allResume[getIndex(uuid)];
-        } catch (Exception e) {
-            System.out.println("Resume is not found");
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            return storage[index];
+        } else {
+            System.out.println("Resume " + uuid + " is not found");
             return null;
         }
     }
 
     private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (allResume[i].getUuid().equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
         }
-        throw new IllegalArgumentException("arguments not found");
+        return -1;
     }
 
     public void delete(String uuid) {
-        boolean find = false;
-        for (int i = 0; i < size; i++) {
-            if (allResume[i].getUuid().equals(uuid) || find) {
-                find = true;
-                allResume[i] = allResume[i + 1];
-            }
-        }
-        if (find) {
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            storage[index] = storage[size-1];
+            storage[size-1] = null;
             size--;
         } else {
-            System.out.println("Resume is not found");
+            System.out.println("Resume " + uuid + " is not found");
         }
     }
 
@@ -67,9 +64,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] copy = new Resume[size];
-        System.arraycopy(allResume, 0, copy, 0, size);
-        return copy;
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
@@ -77,10 +72,11 @@ public class ArrayStorage {
     }
 
     public void update(Resume resume) {
-        try {
-            allResume[getIndex(resume.getUuid())] = resume;
-        } catch (Exception e) {
-            System.out.println("Resume is not found");
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            storage[getIndex(resume.getUuid())] = resume;
+        } else {
+            System.out.println("Resume " + resume.getUuid() + " is not found");
         }
     }
 }
