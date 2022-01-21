a33732060894053974757f7bb1bd9441cfbbde98
MaksimBS <BosUssur@gmail.com>
2022-01-21 22:31:36 +0300

    fix Change on SortedArrayStorage /Template method/

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,32 +2,13 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-    private static final int STORAGE_LIMIT = 10000;
-
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
 
     protected void insertNewResume(Resume Res, int index) {
         storage[size] = Res;
-    }
-
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume " + uuid + " is not found");
-            return null;
-        }
-        return storage[index];
-
     }
 
     protected int findIndex(String uuid) {
@@ -42,25 +23,5 @@ public class ArrayStorage extends AbstractArrayStorage {
     protected void fillDelitedElement(int index) {
         storage[index] = storage[size - 1];
         storage[size - 1] = null;
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public void update(Resume resume) {
-        int index = findIndex(resume.getUuid());
-        if (index < 0) {
-            System.out.println("Resume " + resume.getUuid() + " is not found");
-        } else {
-            storage[index] = resume;
-        }
     }
 }
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -10,30 +10,6 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
-    public void save(Resume resume) {
-        int index = findIndex(resume.getUuid());
-        if (index < 0) {
-            if (size < STORAGE_LIMIT & size == Math.abs(index) - 1) {
-                //запись в конец массива, не выходит за пределы массива
-                storage[size] = resume;
-                size++;
-            } else if (size < STORAGE_LIMIT) {
-                //запись в середину массива
-                int x = Math.abs(index);
-                Resume[] tempStorage = Arrays.copyOfRange(storage, Math.abs(index) - 1, size);
-                storage[Math.abs(index) - 1] = resume;
-                System.arraycopy(tempStorage, 0, storage, Math.abs(index), tempStorage.length);
-                size++;
-            } else {
-                //нет места для записи
-                System.out.println("Resume bank is full");
-            }
-        } else {
-            System.out.println("This resume " + resume.getUuid() + " is already in the bank");
-        }
-    }
-
-    @Override
     protected void insertNewResume(Resume res, int index) {
         if (size == Math.abs(index) - 1) {
             //запись в конец массива, не выходит за пределы массива
