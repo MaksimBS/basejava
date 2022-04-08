c8d52c4a09a60d58fe977566bf9fa610f33f7826
MaksimBS <BosUssur@gmail.com>
2022-04-08 22:37:06 +0300

    Lesson 09 - MainFile

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -1,25 +1,32 @@
 package com.urise.webapp;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class MainFile {
     public static void main(String[] args) {
         File dir = new File("./src/com/urise/webapp");
-        printDirectory(dir);
+        printDirectory(dir,4);
     }
 
-    public static void printDirectory(File dir) {
+    public static void printDirectory(File dir, int Raw) {
         File[] files = dir.listFiles();
 
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("    File: " + file.getName());
+                    System.out.println(buildString('-',Raw) + "File: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    printDirectory(file);
+                    System.out.println(buildString('-',Raw) + "Directory: " + file.getName());
+                    printDirectory(file, Raw*2);
                 }
             }
         }
+    }
+
+    static String buildString(char c, int n) {
+        char[] arr = new char[n];
+        Arrays.fill(arr, c);
+        return new String(arr);
     }
 }
