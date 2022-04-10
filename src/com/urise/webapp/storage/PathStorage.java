648cafb1848d74ec4b747b4130f4029da89ee2d1
MaksimBS <BosUssur@gmail.com>
2022-04-10 22:51:10 +0300

    Lesson 09 - fix Lesson 09

--- src/com/urise/webapp/storage/PathStorage.java
+++ src/com/urise/webapp/storage/PathStorage.java
@@ -97,7 +97,7 @@ public class PathStorage extends AbstractStorage<Path> {
         try {
             return Files.list(directory);
         } catch (IOException e) {
-            throw new StorageException("Path read error", null, e);
+            throw new StorageException("File read error", null, e);
         }
     }
 }
