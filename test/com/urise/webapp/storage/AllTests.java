70d7ce668137019e5747b6f07d27a36d546b6c16
MaksimBS <BosUssur@gmail.com>
2022-04-19 23:01:45 +0300

    Lesson 10 - make JSON StreamSerializer

--- test/com/urise/webapp/storage/AllTests.java
+++ test/com/urise/webapp/storage/AllTests.java
@@ -12,7 +12,8 @@ import org.junit.runners.Suite;
         MapStorageTest.class,
         FileStorageTest.class,
         PathStorageTest.class,
-        XmlPathStorageTest.class
+        XmlPathStorageTest.class,
+        JsonPathStorageTest.class
 })
 public class AllTests {
 }
