9f5e085faea97358f89fc760f7d1206696dd37af
MaksimBS <BosUssur@gmail.com>
2022-05-25 23:19:09 +0300

    Lesson 13 - fix Config and SqlStorageTest

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -1,5 +1,7 @@
 package com.urise.webapp;
 
+import com.urise.webapp.storage.SqlStorage;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,9 +14,10 @@ public class Config {
 
     private Properties props = new Properties();
     private String storageDir;
-    private String dbUrl;
-    private String dbUser;
-    private String dbPassword;
+   // private String dbUrl;
+   // private String dbUser;
+   // private String dbPassword;
+    private SqlStorage storage;
 
     public static Config get() {
         return INSTANCE;
@@ -24,18 +27,23 @@ public class Config {
         try (InputStream is = new FileInputStream(PROPS)) {
             props.load(is);
             storageDir = props.getProperty("storage.dir");
-            dbUrl = props.getProperty("db.url");
-            dbUser = props.getProperty("db.user");
-            dbPassword = props.getProperty("db.password");
+//            dbUrl = props.getProperty("db.url");
+//            dbUser = props.getProperty("db.user");
+//            dbPassword = props.getProperty("db.password");
+            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
     }
 
+    public SqlStorage getStorage() {
+        return storage;
+    }
+
     public String getStorageDir() {
         return storageDir;
     }
-
+/*
     public String getDbUrl() {
         return dbUrl;
     }
@@ -47,4 +55,6 @@ public class Config {
     public String getDbPassword() {
         return dbPassword;
     }
+*/
+
 }
